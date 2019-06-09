488407e6ee7aa23f5c96f83e420a8e8ac9169bcf
NaasirHaleem <49250047+NaasirHaleem@users.noreply.github.com>
2019-06-08 20:24:00 -0500

    Add files via upload

--- Project_1/anecdotes/anecdotes.js
+++ Project_1/anecdotes/anecdotes.js
@@ -1,3 +1,33 @@
+const myAPIEndpoint = 'https://wildside-nu.herokuapp.com/anecdotes';
+const addVote = (ev) => {
+    // 1. get user-generated data:
+    const anecdote = document.querySelector('#anecdote').value;
+    const date = document.querySelector('#date').value;;
+
+fetch(myAPIEndpoint, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "anecdote": anecdote,
+        "date": date
+
+
+
+    })
+})
+.then(response => response.json())
+.then(data => {
+    // 3. print the results to the screen
+    console.log(data);
+});
+ev.preventDefault();
+};
+
+// 4. attach function to button:
+document.querySelector('#saveForm').onclick = addVote;
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
